0408cb4f4f24609f5bc86287f4912d78e8d3ffed
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-22 17:05:17 +0530

    [Ajay: duplicate elements]

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duplicate_elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 2, 1, 1,2 };
+            Array.Sort(arr);
+            int duplicate_count = 0;
+            for (int i=0;i<arr.Length-1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    duplicate_count++;
+                }
+            }
+            Console.WriteLine(duplicate_count);
+            Console.ReadLine();
+
+
+        }
+    }
+}
