008a9c7fd1b39a9b7a0ef1c1fbd2a70883fc291b
Ajaykumar-B-code <bonagiriajaykumar1@gmail.com>
2024-01-23 14:59:12 +0530

    [Ajay] Problem3 frequency of the array

--- Program.cs
+++ Program.cs
@@ -11,21 +11,24 @@ namespace duplicate_elements
     {
         static void Main(string[] args)
         {
-            int[,] a = { { 1, 2, 3 },
-                          { 4, 5,7 } };
-            int sum = 0;
-            for (int i = 0;i < a.GetLength(0) ; i++)
+            int[] a = { 1, 5, 1, 2, 3, 9, 3, 6, 2, 8, 2, 9 };
+            Array.Sort(a);
+            int count = 0, variable = a[0];
+            for (int i = 0; i < a.Length; i++)
             {
-               
-                for (int j=0;j< a.GetLength(1) ; j++)
+                if (variable == a[i])
                 {
-                    
-                    sum += a[i,j];
-                    
+                    count++;
                 }
-                Console.WriteLine($"The sum of the row {i} is {sum}");
-
+                else
+                {
+                    Console.WriteLine($"{variable} : {count}");
+                    count = 1;
+                    variable = a[i];
+                }
             }
+            Console.WriteLine($"{variable} : {count}");
+
             Console.ReadLine();
 
 
